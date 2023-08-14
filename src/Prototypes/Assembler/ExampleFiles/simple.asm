b04efbe7c5de9f766980339a77d6e8cd5e16a345
Tim Kingsmill <tim.kingsmill@gmail.com>
2023-08-14 12:21:52 +0930

    Refactoring the C++ Assembler

--- src/Prototypes/Assembler/ExampleFiles/simple.asm
+++ src/Prototypes/Assembler/ExampleFiles/simple.asm
@@ -1,3 +1,12 @@
-        .text
+.text
 
 main:
+        addi x8, x0, 0
+        addi x9, x0, 0          
+        addi x10, x0, 0 
+        jal x0, exit
+        case2:
+        add x10, x10, x7
+        lw x11, 0(x10)
+
+exit:
